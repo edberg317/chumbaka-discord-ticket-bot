aa8523633c198510f684298d44bddd199980ecfc
edberg317 <cheefan.lee20tecraf@gmail.com>
2022-11-23 14:21:27 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ server.all('/', (req, res) => {
 });
 
 function keepAlive() {
-	server.listen(3000, () => {
+	server.listen(process.env.PORT || 3001, () => {
 		console.log('Server is now ready! | ' + Date.now());
 	});
 }
