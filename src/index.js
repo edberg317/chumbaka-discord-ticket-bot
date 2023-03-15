d585ce90047e246ec11ca1e3a002da534c996498
edberg317 <cheefan.lee20tecraf@gmail.com>
2023-03-16 05:23:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
-process.title = 'Chumbaka Discord Ticket Bot';
-
-const min_node_version = '16.6.0';
+// Load modules
 const semver = require('semver');
-if (semver.lt(process.versions.node, min_node_version)) return console.log(`\x07Error: Chumbaka Discord Ticket Bot does not work on Node v${process.versions.node}; please upgrade to v${min_node_version} or above.`);
-
-require('dotenv').config();
-
-// To catch unhandledRejection or error
-const { version } = require('../package.json');
-process.on('unhandledRejection', error => {
-	console.log('PLEASE INCLUDE THIS INFORMATION IF YOU ASK FOR HELP ABOUT THE FOLLOWING ERROR:');
-	console.log(`Chumbaka Discord Ticket Bot v${version}, Node v${process.versions.node} on ${process.platform}`);
-	console.log('An error was not caught');
-	if (error instanceof Error) {
-		console.log(`Uncaught ${error.name}`);
-	}
-	console.log(error);
-});
-
+const DiscordUtils = require('./utils/discord');
 const ListenerLoader = require('./modules/listeners/loader');
 const CommandManager = require('./modules/commands/manager');
 const TicketManager = require('./modules/tickets/manager');
-
 const { Client, Intents } = require('discord.js');
 const { SheetDatabase } = require('sheets-database');
+
+// Load environment variables from a .env file
+require('dotenv').config();
+
+// Set the process title
+process.title = 'Chumbaka Tickets Bot';
+
+// Check if the Node.js version is greater than or equal to a minimum version
+const min_node_version = '16.6.0';
+if (semver.lt(process.versions.node, min_node_version)) {
+	// If the version is too low, print an error message and exit the program
+	return console.log(`\x07Error: Chumbaka Tickets Bot does not work on Node v${process.versions.node}; please upgrade to v${min_node_version} or above.`);
+}
+
+// Log unhandled rejections and errors with relevant information
+const { version } = require('../package.json');
+process.on('unhandledRejection', error => {
+	console.log('PLEASE INCLUDE THIS INFORMATION IF YOU ASK FOR HELP ABOUT THE FOLLOWING ERROR:');
+	console.log(`Discord Tickets v${version}, Node v${process.versions.node} on ${process.platform}`);
+	console.log('An error was not caught');
+	if (error instanceof Error) console.log(`Uncaught ${error.name}`);
+	console.log(error);
+});
 
 /**
  * The Discord client
@@ -46,29 +51,36 @@ class Bot extends Client {
 			],
 		});
 
+		// Asynchronously initialize the bot
 		(async () => {
+			// Set the version of the bot
 			this.version = version;
 
-			// User configuration
+			// Load user configuration from a separate file
 			this.config = require('../user/config');
 
-			// Log util
+			// Load a log utility from a separate file
 			this.log = require('../user/log');
 
-			// set the max listeners for each event
+			// Set the maximum number of listeners for each event
 			this.setMaxListeners(this.config.max_listeners);
 
+			// Create a utility instance for some utility methods
+			this.utils = new DiscordUtils(this);
+
+			// Load listeners using a custom ListenerLoader class
 			const listeners = new ListenerLoader(this);
-			// load listeners
 			listeners.load();
 
+			// Create a new TicketManager instance for handling tickets
 			/** The ticket manager */
 			this.tickets = new TicketManager(this);
 
+			// Create a new CommandManager instance for handling commands
 			/** The command manager, used by internal and plugin commands */
 			this.commands = new CommandManager(this);
 
-			// Google Spreadsheet DB connection
+			// Create a new Google Sheets Database instance using a sheet ID and credentials
 			this.db = new SheetDatabase(this.config.sheetId);
 			const creds = require('../app-credentials.json');
 			await this.db.useServiceAccount(creds);
@@ -80,10 +92,11 @@ class Bot extends Client {
 			});
 			*/
 
+			// Sync the database
 			await this.db.sync();
 
+			// Log in to the Discord API using a bot token
 			console.log('Connecting to Discord API...');
-
 			this.login(process.env.DISCORD_TOKEN);
 		})();
 	}
